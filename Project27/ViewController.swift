776cd9beae3645012edf2ff9386687b8e3a99cd7
KJM <tdcian@hexlant.com>
2020-01-08 19:48:45 +0900

    Initial Commit

---
+++ Project27/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project27
+//
+//  Created by hexlant_01 on 2020/01/08.
+//  Copyright © 2020 hexlant_01. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
